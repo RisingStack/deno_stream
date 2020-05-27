205373dc9e1c52a52d8dee2c2d669a38eb674861
Peter Czibik <p.czibik@gmail.com>
2020-05-27 19:17:22 +0200

    Fix auth protocol

--- index.ts
+++ index.ts
@@ -1,30 +1,42 @@
 import { varnum } from "https://deno.land/std@0.50.0/encoding/binary.ts";
 
+enum PacketType {
+  SERVERDATA_AUTH = 3,
+  SERVERDATA_AUTH_RESPONSE = 2,
+  SERVERDATA_EXECCOMMAND = 2,
+  SERVERDATA_RESPONSE_VALUE = 0,
+}
+
+const nullTerminator = new Uint8Array(0);
+
 class Packet {
-  constructor(private ID: number, private Type: number, private Body: string) {}
+  constructor(
+    public ID: number,
+    public Type: PacketType,
+    public Body: string,
+  ) {}
 
   toNetwork(): Uint8Array {
     const id = toLEUI32(this.ID);
     const type = toLEUI32(this.Type);
-    const body =
-      this.Body && this.Body.length
-        ? new TextEncoder().encode(this.Body)
-        : new Uint8Array(0);
-    const size = toLEUI32(id.byteLength + type.byteLength + body.byteLength);
-    const end = new Uint8Array(0);
+    const body = this.Body && this.Body.length
+      ? new TextEncoder().encode([this.Body, "\u0000"].join())
+      : new Uint8Array([0]);
 
-    return concatArrayBuffers(size, id, type, body, end);
+    const size = toLEUI32(8 + body.byteLength);
+
+    return concatArrayBuffers(size, id, type, body, nullTerminator);
   }
 
-  static fromNetwork (buf: Uint8Array)  : Packet {
-    return new Packet(1,2,'alma');
+  static fromNetwork(buf: Uint8Array): Packet {
+    return new Packet(1, 2, "alma");
   }
 }
 
 function concatArrayBuffers(...bufs: ArrayBuffer[]) {
   const length = bufs.reduce(
     (length, { byteLength }) => length + byteLength,
-    0
+    0,
   );
 
   const result = new Uint8Array(length);
@@ -41,7 +53,7 @@ function concatArrayBuffers(...bufs: ArrayBuffer[]) {
 function toLEUI32(num: number) {
   const buf = new ArrayBuffer(4);
   const view = new DataView(buf);
-  view.setUint32(0, num, true);
+  view.setInt32(0, num, true);
   return buf;
 }
 
@@ -51,19 +63,50 @@ const connection = await Deno.connect({
   transport: "tcp",
 });
 
-const serverDataAuth = new Packet(123, 3, "he1Quu7eekiecho");
-
+const serverDataAuth = new Packet(
+  123,
+  PacketType.SERVERDATA_AUTH,
+  "aeYoqu2Aeh4see3",
+);
 await connection.write(serverDataAuth.toNetwork());
 
 const authResponseRawSize = new Uint8Array(4);
 await connection.read(authResponseRawSize);
 
-const authResponseSize = varnum(authResponseRawSize, { endian: "little" });
+const authResponseSize = varnum(
+  authResponseRawSize,
+  { endian: "little", dataType: "int8" },
+);
 
 const authResponse = new Uint8Array(authResponseSize!);
 
 await connection.read(authResponse);
-console.log(authResponse);
 
-const response = new TextDecoder().decode(authResponse)
-console.log(response);
+const responsePacketIdRaw = authResponse.slice(0, 4);
+const responsePacketId = varnum(
+  responsePacketIdRaw,
+  { endian: "little", dataType: "int8" },
+);
+
+if (responsePacketId === -1) {
+  throw new Error("Wrong password!");
+}
+
+if (serverDataAuth.ID !== responsePacketId) {
+  throw new Error(
+    `:( IDs are not the same ${serverDataAuth.ID} ${responsePacketId}`,
+  );
+}
+
+const typeRaw = authResponse.slice(4, 8);
+const type = varnum(
+  typeRaw,
+  { endian: "little", dataType: "int8" },
+);
+
+if (type !== PacketType.SERVERDATA_AUTH_RESPONSE) {
+  throw new Error(":(");
+}
+
+const bodyRaw = authResponse.slice(8, authResponse.byteLength - 1);
+new TextDecoder().decode(bodyRaw);
